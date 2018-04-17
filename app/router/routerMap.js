a9dd24bbd3b2830ead15d482a905426411c203c9
18482175910 <495866520@qq.com>
2018-04-17 09:58:21 +0800

    优化

--- app/router/routerMap.js
+++ app/router/routerMap.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter, Route, Switch} from 'react-router-dom'
+import { HashRouter, Route, Switch } from 'react-router-dom'
 import { App } from '../container/app'
 import { Home } from '../container/home/index'
 import { Body } from '../components/body/index'
@@ -11,8 +11,8 @@ import { NotFound } from '../components/notFound/index'
 export class RouterMap extends React.Component {
     render() {
         return (
-        <HashRouter>
-            <App>
+            <HashRouter>
+               <App>
             <Switch>
                 <Route exact path = "/" component ={ Home } ></Route>
                 <Route exact path = "/header" component ={ Header } ></Route>
@@ -20,8 +20,8 @@ export class RouterMap extends React.Component {
                 <Route exact path = "/footer/:id"  component ={ Footer } ></Route>
                 <Route path="/*" component ={ NotFound } ></Route>
             </Switch>
-            </App>App>
-        </HashRouter>
+                </App>             
+            </HashRouter>
         );
     }
 }
