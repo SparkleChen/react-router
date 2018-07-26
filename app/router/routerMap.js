f26105a94e7567426dcbe0199ef320073a06504c
chenlan <chenlan@chenlandeMacBook-Pro.local>
2018-07-26 15:57:58 +0800

    01

--- app/router/routerMap.js
+++ app/router/routerMap.js
@@ -5,6 +5,7 @@ import { Body } from '../components/body/index'
 import { Header } from '../components/header/index'
 import { Footer } from '../components/footer/index'
 import { NotFound } from '../components/notFound/index'
+import { App } from '../container/app';
 
 //路由 render方式渲染
 const NotFounded = ({component:Component, ...rest}) =>{
@@ -18,7 +19,8 @@ return (<Route {...rest} render={
 
 export class RouterMap extends React.Component {
     render() {
-        return (        
+        return (
+            <App>        
             <Switch>
                 <Route exact path = "/" component ={ Home } ></Route>    {/* 路由component 方式渲染 */}
                 <Route exact path = "/header" component ={ Header } ></Route>
@@ -26,6 +28,7 @@ export class RouterMap extends React.Component {
                 <Route exact path = "/footer/:id"  component ={ Footer } ></Route>
                 <NotFounded path='/not' component={ NotFound }></NotFounded>
             </Switch>
+            </App>   
         );
     }
 }
